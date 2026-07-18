00f9c16b7dec14f2d241ecad533f690768a65a6e
rich1308 <rarenas1308@gmail.com>
2026-07-18 17:41:56 -0500

    Used if to get user input and swich to uppercase or lowercase

--- Console_Projects/GetData/src/datosUsuarios/RedingData.java
+++ Console_Projects/GetData/src/datosUsuarios/RedingData.java
@@ -9,14 +9,35 @@ public class RedingData {
 		System.out.println("Welcome to my program");
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Please, Type anything");
+		//Getting data from user
 		String mydata = scan.nextLine();
 		System.out.println("You Typed:  " + mydata );
 		System.out.println("Would you like to convert your data to Uppercase or Lowercase");
 		System.out.println("Please, Type yes or no");
 		String userInput = scan.nextLine();
-		
+		// Creating two options 
 		if(userInput.trim().toLowerCase().equals("yes") || userInput.trim().toLowerCase().equals("y") ) {
 			System.out.println("Press 1 to conver to Uppercase or 2 to Convert to Lowercase");
+			int userchoosing = scan.nextInt();
+			switch (userchoosing) {
+			case 1: {
+				
+				System.out.println("This is the new data obtained");
+				System.out.println(mydata.toUpperCase());
+				
+				break;
+			}
+			case 2: {
+				
+
+				System.out.println("This is the new data obtained");
+				System.out.println(mydata.toLowerCase());
+				
+				break;
+			}
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + userchoosing);
+			}
 			
 		}
 		else {
