2f1b517794afafd7746e81ebf49d3b1f7e347d15
rich1308 <rarenas1308@gmail.com>
2026-07-18 17:15:42 -0500

    Created the main class to scann user input, showing on screen the user input

---
+++ Console_Projects/GetData/src/datosUsuarios/RedingData.java
@@ -0,0 +1,17 @@
+package datosUsuarios;
+
+import java.util.Scanner;
+
+public class RedingData {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to my program");
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please, Type anything");
+		String mydata = scan.nextLine();
+		System.out.println("You Typed:  " + mydata );
+		
+	}
+
+}
