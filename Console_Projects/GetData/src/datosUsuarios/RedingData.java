b38e362b2bb0c1ec0f78e8350be36f9586556776
rich1308 <rarenas1308@gmail.com>
2026-07-18 17:29:31 -0500

    Created a conditional to show user two options to convert data to uppercase to lowercase

--- Console_Projects/GetData/src/datosUsuarios/RedingData.java
+++ Console_Projects/GetData/src/datosUsuarios/RedingData.java
@@ -11,6 +11,18 @@ public class RedingData {
 		System.out.println("Please, Type anything");
 		String mydata = scan.nextLine();
 		System.out.println("You Typed:  " + mydata );
+		System.out.println("Would you like to convert your data to Uppercase or Lowercase");
+		System.out.println("Please, Type yes or no");
+		String userInput = scan.nextLine();
+		
+		if(userInput.trim().toLowerCase().equals("yes") || userInput.trim().toLowerCase().equals("y") ) {
+			System.out.println("Press 1 to conver to Uppercase or 2 to Convert to Lowercase");
+			
+		}
+		else {
+			System.out.println("You did not chose any correct answer");
+		}
+		
 		
 	}
 
